9337f9d0f3d5094ce2a04a62b13b80bc34437f7d
zhoujuntao <350529307@qq.com>
2017-04-02 19:40:14 +0800

    6.15

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -36,6 +36,25 @@
            <div class="star-wrapper">
              <star :size='48' :score='seller.score'></star>
            </div>
+           <div class="title">
+             <div class="line"></div>
+             <div class="text">优惠信息</div>
+             <div class="line"></div>
+           </div>
+           <ul v-if="seller.supports" class="supports">
+             <li class="supports-item" v-for="(item,index) in seller.supports">
+               <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+               <span class="text">{{seller.supports[index].description}}</span>
+             </li>
+           </ul>
+           <div class="title">
+             <div class="line"></div>
+             <div class="text">商家公告</div>
+             <div class="line"></div>
+           </div>
+           <div class="bulletin">
+             <p class="content">{{seller.bulletin}}</p>
+           </div>
          </div>
        </div>
        <div class="detail-close">
@@ -221,6 +240,65 @@ export default {
             margin-top: 18px;
             padding:2px 0;
             text-align: center;
+          }
+          .title {
+            width: 80%;
+            margin: 30px auto 24px auto;
+            display: flex;
+            .line {
+              flex:1;
+              position: relative;
+              top: -6px;
+              border-bottom: 1px solid rgba(255,255,255,0.5);
+            }
+            .text {
+              padding:0 12px;
+              font-size: 14px;
+              font-weight: 700;
+            }
+          }
+          .supports {
+            width: 80%;
+            margin: 0 auto;
+            .supports-item {
+              padding:0 12px;
+              margin-bottom: 12px;
+              font-size: 0;
+              &:last-child {
+                margin-bottom: 0;
+              }
+              .icon {
+                display: inline-block;
+                width: 16px;
+                height: 16px;
+                vertical-align: top;
+                margin-right: 6px;
+                background-size: 16px 16px;
+                background-repeat: no-repeat;
+                &.decrease {
+                  background-image: url("../../assets/img/decrease_1@2x.png")
+                }
+                &.discount {
+                  background-image: url("../../assets/img/discount_1@2x.png")
+                }
+                &.gurantee {
+                  background-image: url("../../assets/img/guarantee_1@3x.png")
+                }
+                &.invoice {
+                  background-image: url("../../assets/img/invoice_1@2x.png")
+                }
+                &.special {
+                  background-image: url("../../assets/img/special_1@2x.png")
+                }
+              }
+              .text {
+                line-height: 12px;
+                font-size: 12px;
+              }
+            }
+          }
+          .bulletin {
+            
           }
           .detail-close {
             position: relative;
