179e98d6bd4f2bfb060ae7d515de18b761c8dd9b
zhoujuntao <350529307@qq.com>
2017-04-06 20:40:44 +0800

    7.10

--- src/components/shopcar/shopcar.vue
+++ src/components/shopcar/shopcar.vue
@@ -84,6 +84,7 @@ export default {
           border-right: 1px solid rgba(255,255,255,0.1);
           font-size: 16px;
           font-weight: 700;
+          
         }
         .desc {
           display: inline-block;
