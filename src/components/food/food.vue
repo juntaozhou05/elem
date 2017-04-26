1e5b4b7939bb77445c86e8e7ced1f891613fc163
juntaozhou05 <350529307@qq.com>
2017-04-26 12:05:05 +0800

    8.4

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,12 +1,33 @@
 <template>
     <transition name="move">
-      <div v-show="showFlag" class="food">
-        <div>111111</div>
+      <div v-show="showFlag" class="food" ref="food">
+        <div class="image-header">
+          <img :src="food.image">
+          <div class="back" @click="hide">
+            <span>&lt;</span>
+          </div>
+        </div>
+        <div class="content">
+          <h1 class="title">{{food.name}}</h1>
+          <div class="detail">
+            <span class="sell-count">月售{{food.sellCount}}份</span>
+            <span class="rating">好评率{{food.rating}}</span>
+          </div>
+          <div class="price">
+            <span class="now">￥{{food.price}}</span>
+            <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+          </div>
+        </div>
+        <div class="cartcontrol-wrapper">
+          <cartcontrol></cartcontrol>
+        </div>
       </div>
     </transition>
 </template>
 
 <script>
+import BScroll from 'better-scroll';
+import cartcontrol from '../cartcontrol/cartcontrol.vue'
 export default {
   props:{
     food:{
@@ -21,8 +42,23 @@ export default {
   methods: {
     show() {
       this.showFlag = true;
+      this.$nextTick(()=>{
+        if(!this.scroll) {
+          this.scroll = new BScroll(this.$refs.food, {
+            click:true
+          })
+        }else {
+          this.scroll.refresh();
+        }
+      })
+    },
+    hide() {
+      this.showFlag = false;
     }
-  }   
+  },
+  components: {
+    cartcontrol
+  }  
 }
 </script>
 
@@ -41,6 +77,67 @@ export default {
     }
     &.move-enter, &.move-leave-active {
       transform:translate3D(100%,0,0);
+    }
+    .image-header {
+      position: relative;
+      width: 100%;
+      height: 0;
+      padding-top: 100%;
+      img {
+        position: absolute;
+        top:0;
+        left:0;
+        width: 100%;
+        height: 100%;
+      }
+      .back {
+        position: absolute;
+        top:10px;
+        left:0;
+        span {
+          font-weight: bold;
+          color:white;
+          padding-left: 10px;
+          font-size: 20px;
+        }
+      }
+    }
+    .content {
+      padding:18px;
+      .title {
+        line-height: 14px;
+        margin-bottom: 8px;
+        font-size: 14px;
+        font-weight: 700;
+        color:rgb(7,17,27);
+      }
+      .detail {
+        margin-bottom: 18px;
+        line-height: 10px;
+        font-size: 0;
+        height: 10px;
+        .sell-count, .rating {
+          font-size: 10px;
+          color:rgb(147,153,159);
+        }
+        .sell-count {
+          margin-right: 12px;
+        }
+      }
+      .price {
+            font-weight: 700;
+            line-height: 24px;
+            .now {
+              margin-right: 8px;
+              font-size: 14px;
+              color:rgb(240,20,20);
+            }
+            .old {
+              text-decoration: line-through;
+              font-size: 10px;
+              color:rgb(147,153,159);
+            }
+          }
     }
   }
 </style>
